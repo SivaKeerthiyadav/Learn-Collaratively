0af7b8f945b7e0e758090dac5104d1adf4039198
Mounikabandi3 <144351471+Mounikabandi3@users.noreply.github.com>
2024-05-21 16:04:28 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = "";
+$database = "mst"; // Database name
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
